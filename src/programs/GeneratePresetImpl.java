0588a6994e1edcd7713f9f6b053cfb2e1560ef86
VanHellthing <vanhellthing@mail.ru>
2025-01-13 22:59:48 +0300

    Generate Army realisation

--- src/programs/GeneratePresetImpl.java
+++ src/programs/GeneratePresetImpl.java
@@ -10,7 +10,35 @@ public class GeneratePresetImpl implements GeneratePreset {
 
     @Override
     public Army generate(List<Unit> unitList, int maxPoints) {
-        // Ваше решение
-        return null;
+        Army enemyArmy = new Army();
+        List<Unit> listOfUnits = new ArrayList<>();
+        int totalPoints = 0;
+
+        sortUnits(unitList);
+
+        for (Unit unit : unitList) {
+            int maxUnitsForType = Math.min(11, maxPoints / unit.getCost());
+            for (int i = 0; i < maxUnitsForType && totalPoints + unit.getCost() <= maxPoints; i++) {
+                listOfUnits.add(createUnit(unit));
+            }
+        }
+
+        enemyArmy.setUnits(listOfUnits);
+        enemyArmy.setPoints(totalPoints);
+        return enemyArmy;
     }
+
+    private Unit createUnit (Unit unit) {
+        Unit newUnit = new Unit(unit.getName(), unit.getUnitType(), unit.getHealth(),
+                unit.getBaseAttack(), unit.getCost(), unit.getAttackType(),
+                unit.getAttackBonuses(), unit.getDefenceBonuses(), unit.getxCoordinate(), unit.getyCoordinate());
+        newUnit.setName(unit.getUnitType());
+        return newUnit;
+    }
+
+    private void sortUnits(List<Unit> unsortedUnits) {
+        unsortedUnits.sort(Comparator.comparingDouble(
+                unit -> -((double) unit.getBaseAttack() / unit.getCost() + (double) unit.getHealth() / unit.getCost())));
+    }
+
 }
